a04d95f6e6361d66bbd112ad605b832be97a01f7
Zilezia <151420333+Zilezia@users.noreply.github.com>
2023-11-29 18:17:49 +0000

    Add files via upload

---
+++ normal/script.js
@@ -0,0 +1,60 @@
+const inputBox = document.getElementById("input-box");
+const inputBtn = document.getElementById("input-btn");
+const listContainer = document.getElementById("list-container");
+
+function addTask()
+{
+    if(inputBox.value === "")
+    {
+        alert("You must type something");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+inputBtn.addEventListener("click", () => {
+    addTask();
+});
+inputBox.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      addTask();
+    }
+  });
+
+listContainer.addEventListener("click", function(e) 
+{
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN")
+    {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+
+  
+
+function saveData()
+{
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask()
+{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
